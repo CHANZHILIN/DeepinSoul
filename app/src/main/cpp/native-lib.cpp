2a420f0b44667c3e4cfacc336cd76752594373cd
chanzhilin <1181785848@qq.com>
2019-06-11 11:29:35 +0800

    new soul_login

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_deepinsoul_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "nihao ,this  is Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
