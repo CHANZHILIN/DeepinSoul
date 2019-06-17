2f07060025c72e47378a998acb5a822f7bdd0f10
chanzhilin <1181785848@qq.com>
2019-06-17 17:59:50 +0800

    SnackbarUtil

--- app/src/main/java/com/deepinsoul/MainActivity.kt
+++ app/src/main/java/com/deepinsoul/MainActivity.kt
@@ -2,17 +2,21 @@ package com.deepinsoul
 
 import android.Manifest
 import android.content.Context
+import android.view.KeyEvent
 import android.widget.Toast
 import com.kotlin_baselib.base.BaseActivity
 import com.kotlin_baselib.base.EmptyModelImpl
 import com.kotlin_baselib.base.EmptyPresenterImpl
 import com.kotlin_baselib.base.EmptyView
 import com.kotlin_baselib.glide.GlideUtil
+import com.kotlin_baselib.utils.SnackbarUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import pub.devrel.easypermissions.EasyPermissions
 
 class MainActivity : BaseActivity<EmptyView, EmptyModelImpl, EmptyPresenterImpl>(), EmptyView,
     EasyPermissions.PermissionCallbacks {
+
+    private var mFirstTime: Long = 0
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
@@ -62,7 +66,7 @@ class MainActivity : BaseActivity<EmptyView, EmptyModelImpl, EmptyPresenterImpl>
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.READ_EXTERNAL_STORAGE
             );
-        }else{
+        } else {
             GlideUtil.instance.loadImageWithProgress(
                 mContext as Context,
 //            "https://www.4hou.com/uploads/20180818/1534558145264870.png",
@@ -74,6 +78,21 @@ class MainActivity : BaseActivity<EmptyView, EmptyModelImpl, EmptyPresenterImpl>
 
     override fun initListener() {
 
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event?.action == KeyEvent.ACTION_DOWN) {
+            if (System.currentTimeMillis() - mFirstTime > 2000) {
+                mFirstTime = System.currentTimeMillis()
+                SnackbarUtil.ShortSnackbar(
+                    this.window.decorView.rootView,
+                    getString(R.string.click_one_more_time_to_exit),
+                    SnackbarUtil.Alert
+                ).show()
+                return false
+            }
+        }
+        return super.onKeyDown(keyCode, event)
     }
 
     /**
