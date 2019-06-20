543931273508037c849d7c0139efee3da0b33a70
chanzhilin <1181785848@qq.com>
2019-06-20 14:42:44 +0800

    基本页面

--- app/src/main/java/com/deepinsoul/MainActivity.kt
+++ app/src/main/java/com/deepinsoul/MainActivity.kt
@@ -1,25 +1,39 @@
 package com.deepinsoul
 
 import android.Manifest
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.GravityCompat
+import android.support.v4.view.ViewPager
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
+import android.util.DisplayMetrics
+import android.util.SparseArray
 import android.view.KeyEvent
 import android.view.MenuItem
-import android.widget.Toast
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import com.kotlin_baselib.base.BaseActivity
 import com.kotlin_baselib.base.EmptyModelImpl
 import com.kotlin_baselib.base.EmptyPresenterImpl
 import com.kotlin_baselib.base.EmptyView
+import com.kotlin_baselib.utils.Dp2PxUtil
 import com.kotlin_baselib.utils.SnackbarUtil
+import com.soul_picture.PictureFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import pub.devrel.easypermissions.EasyPermissions
 
+/**
+ *  Created by CHEN on 2019/6/12
+ *  Email:1181785848@qq.com
+ *  Package:com.deepinsoul
+ *  Introduce:
+ **/
 class MainActivity : BaseActivity<EmptyView, EmptyModelImpl, EmptyPresenterImpl>(), EmptyView,
-        EasyPermissions.PermissionCallbacks {
+    EasyPermissions.PermissionCallbacks, RadioGroup.OnCheckedChangeListener {
 
-    /*   private val mFragments = SparseArray<Fragment>()
-       private val rbIdList = intArrayOf(R.id.rb_picture, R.id.rb_music, R.id.rb_video)*/
+    private val mFragments = SparseArray<Fragment>()
+    private val rbIdList = intArrayOf(R.id.rb_picture, R.id.rb_music, R.id.rb_video)
 
     private var mFirstTime: Long = 0
 
@@ -30,7 +44,11 @@ class MainActivity : BaseActivity<EmptyView, EmptyModelImpl, EmptyPresenterImpl>
     }
 
     override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>?) {
-        Toast.makeText(mContext, "权限拒绝", Toast.LENGTH_SHORT).show()
+        SnackbarUtil.ShortSnackbar(
+            window.decorView,
+            "拒绝权限",
+            SnackbarUtil.Alert
+        ).show()
     }
 
     override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>?) {
@@ -59,25 +77,25 @@ class MainActivity : BaseActivity<EmptyView, EmptyModelImpl, EmptyPresenterImpl>
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val actionBarDrawerToggle =
-                ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close)
+            ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close)
         actionBarDrawerToggle.syncState()
         drawer.setDrawerListener(actionBarDrawerToggle)
 //        sample_text.text = stringFromJNI()
         showLoading()
         if (!EasyPermissions.hasPermissions(
-                        this,
-                        Manifest.permission.INTERNET,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        Manifest.permission.READ_EXTERNAL_STORAGE
-                )
+                this,
+                Manifest.permission.INTERNET,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
         ) {
             EasyPermissions.requestPermissions(
-                    this,
-                    "网络权限",
-                    1001,
-                    Manifest.permission.INTERNET,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
+                this,
+                "网络权限",
+                1001,
+                Manifest.permission.INTERNET,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
             );
         } else {
             /* GlideUtil.instance.loadImageWithProgress(
@@ -86,8 +104,60 @@ class MainActivity : BaseActivity<EmptyView, EmptyModelImpl, EmptyPresenterImpl>
                      "http://k.zol-img.com.cn/sjbbs/7692/a7691515_s.jpg",
                      progress_image
              )*/
+            setFragments();
         }
     }
+
+    private fun setFragments() {
+        mFragments.put(0, PictureFragment.newInstance("picture"))
+        mFragments.put(1, PictureFragment.newInstance("music"))
+        mFragments.put(2, PictureFragment.newInstance("video"))
+        val adapter = object : FragmentPagerAdapter(supportFragmentManager) {
+            override fun getItem(i: Int): Fragment {
+                return mFragments.get(i)
+            }
+
+            override fun getCount(): Int {
+                return mFragments.size()
+            }
+        }
+        rg_main.setOnCheckedChangeListener(this)
+        scrollViewPager.adapter = adapter
+        scrollViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                var i = 0
+                for (id in rbIdList) {
+                    if (position == i) {
+                        rg_main.check(id)
+                    }
+                    i++
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+        scrollViewPager.offscreenPageLimit = 2
+    }
+
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        var i = 0
+        for (id in rbIdList) {
+            if (id == checkedId) {
+                scrollViewPager.setCurrentItem(i, true)
+                findViewById<RadioButton>(id).textSize = 20f
+            } else {
+                findViewById<RadioButton>(id).textSize = 16f
+            }
+            i++
+        }
+    }
+
 
     override fun initListener() {
     }
@@ -103,15 +173,16 @@ class MainActivity : BaseActivity<EmptyView, EmptyModelImpl, EmptyPresenterImpl>
             if (System.currentTimeMillis() - mFirstTime > 2000) {
                 mFirstTime = System.currentTimeMillis()
                 SnackbarUtil.ShortSnackbar(
-                        window.decorView,
-                        getString(R.string.click_one_more_time_to_exit),
-                        SnackbarUtil.Alert
+                    window.decorView,
+                    getString(R.string.click_one_more_time_to_exit),
+                    SnackbarUtil.Alert
                 ).show()
                 return false
             }
         }
         return super.onKeyDown(keyCode, event)
     }
+
 
 /*    */
     /**
